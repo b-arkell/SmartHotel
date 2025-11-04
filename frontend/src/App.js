83b6dfc4c2c4b1adb0941fe59dc78d9c44fe2917
Matthew Romano <matbat2205@gmail.com>
2025-11-04 18:06:19 -0500

    Added GuestHub
    Added the guest hub

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,16 @@
-/* import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DeviceHub from "./DeviceHub";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<DeviceHub />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-*/
-
-
-
-// THIS IS FOR TESTING // ABOVE IS THE REAL CODE //
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HotelHub from "./HotelHub";
+import GuestHub from "./GuestHub";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HotelHub />} />
+        <Route path="/hotel" element={<HotelHub />} />
+        <Route path="/guest" element={<GuestHub />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
