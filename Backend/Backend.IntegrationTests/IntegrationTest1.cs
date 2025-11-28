f7bd1ec8f8eeac7d33009c6d500c19e5b8d1587f
b-arkell <broarkell2@gmail.com>
2025-11-28 15:57:21 -0500

    Added MSTests for integration testing
    added scaffolding for integration tests

---
+++ Backend/Backend.IntegrationTests/IntegrationTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.IntegrationTests
+{
+    [TestClass]
+    public sealed class IntegrationTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
